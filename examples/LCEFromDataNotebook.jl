c1d7ba7b89c484cc8087c6b001ca628fb877639a
jClugstor <jadonclugston@gmail.com>
2024-08-22 15:09:58 -0400

    notebook

--- examples/LCEFromDataNotebook.jl
+++ examples/LCEFromDataNotebook.jl
@@ -5,7 +5,31 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ c6b1ad98-60b8-11ef-12c9-7f6a2729e4b3
+using Pkg 
+
+# ╔═╡ 77862b58-3162-420c-b34e-183d9337b564
+Pkg.add(url="https://github.com/jClugstor/LCEFromData.jl")
+
+# ╔═╡ b44b2a30-d948-44b9-8804-8517eca4ce3c
+using LCEFromData
+
+# ╔═╡ 735bb737-de5d-44d5-aaa5-f31246b98b09
+md"""
+Because my package isn't in the general registry yet, you can just add it directly from my GitHub.
+"""
+
+# ╔═╡ 2cf1e8fe-7797-4dc8-85b4-c3a41f1445d6
+md"""
+We'll use DynamicalSystems.jl to generate some data to use.
+"""
+
+# ╔═╡ 4cd5e33e-964c-46cb-96d5-a868a328bfcf
 
 
 # ╔═╡ Cell order:
 # ╠═c6b1ad98-60b8-11ef-12c9-7f6a2729e4b3
+# ╟─735bb737-de5d-44d5-aaa5-f31246b98b09
+# ╠═77862b58-3162-420c-b34e-183d9337b564
+# ╠═b44b2a30-d948-44b9-8804-8517eca4ce3c
+# ╠═2cf1e8fe-7797-4dc8-85b4-c3a41f1445d6
+# ╠═4cd5e33e-964c-46cb-96d5-a868a328bfcf
